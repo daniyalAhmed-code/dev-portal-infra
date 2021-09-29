ebc6a6bac314e57e80a2045cbe1769a1c1b4f5bd
Daniyal Lodhi <daniyal@eurustechnologies.com>
2021-09-30 01:19:03 +0500

    adding variables

---
+++ vars.tf
@@ -0,0 +1,26 @@
+
+variable "ENABLE_FEEDBACK_SUBMISSION"{}
+
+
+variable "DEVELOPMENT_MODE"{}
+
+variable "NODE_ENV"{}
+
+variable "ENV"{}
+variable "ACCOUNT_REGISTRATION_MODE"{}
+
+variable "CUSTOM_DOMAIN_NAME"{}
+
+
+variable "COGNITO_ADMIN_GROUP_DESCRIPTION"{}
+variable "COGNITO_REGISTERED_GROUP_DESCRIPTION"{}
+
+variable "HOSTED_ZONE_ID"{}
+variable "USE_ROUTE53"{}
+
+variable "ORIGIN_ID"{}
+variable "ACM_CERTIFICATE_ARN"{}  
+
+variable "COGNITO_USER_POOL_DOMAIN"{}
+
+variable "ALLOW_ADMIN_CREATE_USER_ONLY"{}
