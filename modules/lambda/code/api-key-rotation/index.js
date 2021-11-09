c5459f207648855f2c303369e541533a1e6fc88b
Daniyal Lodhi <daniyal@eurustechnologies.com>
2021-11-10 04:46:11 +0500

    added key rotation

---
+++ modules/lambda/code/api-key-rotation/index.js
@@ -0,0 +1,45 @@
+const AWS = require('aws-sdk')
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const lambda = new AWS.Lambda();
+const customersController = require('dev-portal-common/customers-controller')
+
+async function invokeLambda(payload) {
+    const cognitoIdentityId = payload.Id
+    const data = await new Promise((resolve, reject) => {
+    customersController.getApiKeyForCustomer(cognitoIdentityId, reject, resolve)
+  })
+    payload['api_data'] = data.items[0]
+    const params = {
+        FunctionName: process.env.InvokeLambdaFunction,
+        InvocationType: 'Event',
+        Payload: JSON.stringify(payload)
+    };
+
+    return new Promise((resolve, reject) => {
+
+        lambda.invoke(params, (err,data) => {
+            if (err) {
+                console.log(err, err.stack);
+                reject(err);
+            }
+            else {
+                console.log(data);
+                resolve(data);
+            }
+        });     
+    });
+}
+
+
+exports.handler = async (event, context) => {
+    let table_name = process.env.CustomersTableName
+    let params = {
+        TableName: table_name
+      };
+    let response = await dynamodb.scan(params).promise()
+    for (let item of response.Items)
+    {
+        await invokeLambda(item);
+    }
+    context.done();
+};
