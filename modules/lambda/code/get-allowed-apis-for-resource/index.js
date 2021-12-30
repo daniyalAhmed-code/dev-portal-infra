1b4e8e4437186921540c003f4dd80ff52dd01d29
daniyalAhmed-code <70039650+daniyalAhmed-code@users.noreply.github.com>
2021-12-31 03:07:26 +0500

    fixing get allowed apis

--- modules/lambda/code/get-allowed-apis-for-resource/index.js
+++ modules/lambda/code/get-allowed-apis-for-resource/index.js
@@ -7,8 +7,13 @@ const Joi = require('joi');
 const rh   =  require('dev-portal-common/responsehandler')
 
 exports.handler = async (req, res) => {
-
-      let listAllResourceType = await customersController.getAllResourceDetails()
-      console.log(listAllResourceType)
-      return rh.callbackRespondWithJsonBody(200,listAllResourceType)
-    }   
+   
+    if(typeof req.body == "string")
+    req['body'] = JSON.parse(req.body)
+    let ResourceId = req.pathParameters.ResourceId
+   
+    const UsagePlanPermission = await customersController.getAllowedApisForResource(
+    ResourceId,
+    )
+    return rh.callbackRespondWithJsonBody(200,UsagePlanPermission)
+}   
